653af56d55d42d4f678948594a4af77f2ef8c2cc
kugga-no1 <850300117@qq.com>
2021-08-15 14:58:45 +0800

    bfs打印二叉树 正序逆序交替

--- Algorithm/src/subject/SearchAndBacktrackingKind/LevelOrder.java
+++ Algorithm/src/subject/SearchAndBacktrackingKind/LevelOrder.java
@@ -90,4 +90,41 @@ public class LevelOrder {
         }
         return resArray;
     }
+/**
+ * @question:请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其层次遍历结果：
+ *
+ * [
+ *   [3],
+ *   [20,9],
+ *   [15,7]
+ * ]
+ * @思路:和之前大体类似，只是要正序一遍逆序一遍打印而已，方法有很多：1。遍历时加个奇偶层判断  2.按之前一样打印 打印完后再对结果做处理（这里写的就是这种）
+ */
+    public List<List<Integer>> levelOrder3(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root != null) queue.add(root);
+        while(!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            for(int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            if(res.size() % 2 == 1) Collections.reverse(tmp);
+            res.add(tmp);
+        }
+        return res;
+    }
+
 }
