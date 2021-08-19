7e17584eeb831d3d61146f080a46c01a9494919d
kugga-no1 <850300117@qq.com>
2021-08-19 09:51:36 +0800

    剑指46  把数字翻译成字符串

--- Algorithm/src/subject/DynamicProgramming/DynamicProgram.java
+++ Algorithm/src/subject/DynamicProgramming/DynamicProgram.java
@@ -1,6 +1,8 @@
 package subject.DynamicProgramming;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @program: DynamicProgram
@@ -140,8 +142,63 @@ public class DynamicProgram {
         return grid[grid.length-1][grid[0].length-1];
     }
 
+
+    /**
+     * @Description:剑指 Offer 46. 把数字翻译成字符串
+     * 给定一个数字，我们按照如下规则把它翻译为字符串：0 翻译成 “a” ，1 翻译成 “b”，……，11 翻译成 “l”，……，25 翻译成 “z”。一个数字可能有多个翻译。请编程实现一个函数，用来计算一个数字有多少种不同的翻译方法。
+     * 示例 1:
+     * 输入: 12258
+     * 输出: 5
+     * 解释: 12258有5种不同的翻译，分别是"bccfi", "bwfi", "bczi", "mcfi"和"mzi"
+     * @return:
+     */
+    public static int translateNum(int num) {
+        String s=String.valueOf(num);
+        ArrayList<Integer> numList = new ArrayList<>();
+        for(int i=0;i<=s.length()-1;i++){
+            numList.add(Integer.valueOf(s.charAt(i)+""));
+        }
+        if(numList.size()==1){
+            return 1;
+        }
+        int a=1,b=1,tmp=1;
+        for(int i=1;i<=numList.size()-1;i++){
+            int mightAdd=0;
+            if(numList.get(i-1)>0&&((10*numList.get(i-1)+numList.get(i))<=25)){
+                mightAdd=a;
+            }
+            tmp=b+mightAdd;
+            a=b;
+            b=tmp;
+        }
+        return tmp;
+    }
+/**
+ * @Description: 剑指 Offer 46. 把数字翻译成字符串优化  不再使用list
+ * @return:
+ */
+    public static int translateNum2(int num) {
+        String s=String.valueOf(num);
+        if(s.length()==1){
+            return 1;
+        }
+        int a=1,b=1,tmp=1;
+        for(int i=1;i<=s.length()-1;i++){
+            int mightAdd=0;
+            if(s.substring(i-1,i+1).compareTo("25")<=0&&s.substring(i-1,i+1).compareTo("10")>=0){
+                mightAdd=a;
+            }
+            tmp=b+mightAdd;
+            a=b;
+            b=tmp;
+        }
+        return tmp;
+    }
+
+
     public static void main(String[] args) {
-    int[][] grid=new int[][]{{1,3,1},{1,5,1},{4,2,1}};
-        System.out.println(maxValue2(grid));
+//    int[][] grid=new int[][]{{1,3,1},{1,5,1},{4,2,1}};
+//        System.out.println(maxValue2(grid));
+        System.out.println(translateNum2(1222222));
     }
 }
